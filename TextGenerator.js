0a3e54759fd8a39cec36fdf07167184f5060e444
Jose Fernando Casadiego Mora <jose.fernando.casadiego@gmail.com>
2023-02-05 19:43:17 -0500

    Create the list of recomendations for eating habits, sleeping and exercise

---
+++ TextGenerator.js
@@ -0,0 +1,54 @@
+
+//in this part write the list of good eating habits, good sport practice for a healty life style and 
+
+const listGoodEatingHabits =[
+    "Evitar los alimentos procesados y ricos en grasas y azúcares.", 
+    "Hacer 5 comidas al día, incluyendo desayuno, almuerzo, cena y dos snacks.", 
+    "Evitar el exceso de sal y azúcares añadidos.",
+    "Incluir proteínas magras en cada comida, como pollo, pescado o tofu.", 
+    "Comer porciones controladas y no saltarse comidas.", 
+    "Beber suficiente agua y evitar bebidas azucaradas.", 
+    "Evitar comer después de cierta hora de la noche.", 
+    "Comer frutas y verduras en cada comida.",
+    "Escuchar a tu cuerpo y no comer más de lo que necesitas.",
+    "Comer una variedad de alimentos frescos y saludables.",];
+
+const listGoodExercisePractice =[
+    "Hacer un calentamiento y estiramiento antes y después de cada sesión de ejercicios.",
+    "Mantener un ritmo cardíaco saludable durante el ejercicio.",
+    "Hacer ejercicio con regularidad, como por lo menos 3 veces por semana.",
+    "Combinar diferentes tipos de ejercicios, como cardio y entrenamiento de fuerza.",
+    "Trabajar en la mejora de la flexibilidad y la postura.",
+    "Establecer objetivos realistas y medirlos para seguir progresando.",
+    "Variar la rutina de ejercicios para evitar la monotonía.",
+    "Incorporar actividad física en la rutina diaria, como caminar o subir escaleras en lugar de tomar el ascensor.",
+    "Buscar la asistencia profesional si es necesario, como un entrenador personal o un fisioterapeuta.",
+    "Elegir actividades que se disfruten, como caminar, correr, nadar o bailar.",];
+
+const listGoodSleepingHabits =[
+    "Establecer una rutina regular de sueño y asegurarse de dormir la cantidad recomendada de horas por noche, que es entre 7 y 9 horas.",
+    "Crear un ambiente de sueño saludable, controlando la temperatura, la luz y el ruido de la habitación.",
+    "Evitar la exposición a la luz azul emitida por dispositivos electrónicos antes de dormir.,",
+    "Incorporar actividades relajantes en la rutina antes de dormir, como la lectura o la meditación.",
+    "Reducir el consumo de estimulantes como cafeína, alcohol y alimentos picantes antes de dormir.",
+    "Mantener un horario de sueño regular y consistente.",
+    "Practicar técnicas de relajación o estiramientos antes de dormir.",
+    "Evitar el uso de dispositivos electrónicos en la cama.",
+    "Proporcionar una superficie de descanso cómoda y adecuada, incluyendo un colchón y almohadas de buena calidad.",
+    "Buscar ayuda profesional si se experimentan problemas de sueño persistentes, como insomnio o apnea del sueño."];
+
+
+const ramdomTextItem = (list)=>{
+    let item = Math.floor(Math.random()*10);
+    return list[item];
+};
+
+
+
+
+
+const randomText =`sdasdasdas:\n
+    
+    
+
+console.log(randomText); 
